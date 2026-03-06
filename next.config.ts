cf899d0a533af17027b9cf0a831642b0e8baa853
CYRILLE TAMBOUG <TAMBOUG@SCECAM.LAN>
2026-03-06 18:21:42 +0100

    fix: production deploy bypass and cleanup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
+    // Cela va permettre de passer l'étape "Running TypeScript" même s'il y a des erreurs
     ignoreBuildErrors: true,
   },
   eslint: {
